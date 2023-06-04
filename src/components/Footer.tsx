ef4059be5333cdd490d2f4ed2e0e5c1c293a2e3f
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2023-06-04 15:31:15 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="w-full py-4 flex items-center justify-center bg-gray-700 border-t border-gray-600">
+    <footer className="absolute inset-x-0 bottom-0 w-full py-4 flex  items-center justify-center bg-gray-700 border-t border-gray-600">
       <p>Feito com 🧡 por</p> 
       <a href="https://www.linkedin.com/in/lilian-martins-fritzen/">
         <strong className="pl-1">
